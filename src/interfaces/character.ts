6adccdd98fc247a08bec7d46af09214fcdef7af3
Leandro Licata <leandrolicata1@gmail.com>
2024-05-11 14:22:06 -0300

    add(-): create component character interface

---
+++ src/interfaces/character.ts
@@ -0,0 +1,15 @@
+export interface Character {
+  name: string;
+  eye_color: string;
+  gender: string;
+}
+
+export interface CharacterDetail {
+  name: string;
+  eye_color: string;
+  birth_year: string;
+  hair_color: string;
+  height: string;
+  skin_color: string;
+  mass: string;
+}
