87f55888eec92554497ac9db83153afcae4dccee
Leandro Licata <leandrolicata1@gmail.com>
2024-05-12 10:59:47 -0300

    fix(-): fix api url

--- src/lib/features/movie/movieThunk.ts
+++ src/lib/features/movie/movieThunk.ts
@@ -2,14 +2,15 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetchMovies = createAsyncThunk("movies/fetchMovies", async () => {
-  const response = await axios.get("/films");
-  return response.data;
+  const response = await axios.get("https://swapi.dev/api/films/");
+  console.log("response: ",response.data.results)
+  return response.data.results;
 });
 
 export const fetchMovieById = createAsyncThunk(
   "movies/fetchMovieById",
   async (id: number) => {
-    const response = await axios.get(`/films/${id}`);
+    const response = await axios.get(`https://swapi.dev/api/films/${id}`);
     return response.data;
   }
 );
