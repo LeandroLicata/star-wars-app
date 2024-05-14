a1f010a087a903043137167295d9153921405f14
Leandro Licata <leandrolicata1@gmail.com>
2024-05-14 10:49:57 -0300

    fix(-): fix FilmPage styles

--- src/app/films/page.tsx
+++ src/app/films/page.tsx
@@ -7,7 +7,7 @@ const FilmsPage = () => {
   const { movies } = useMovies();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black bg-opacity-90 py-4">
+    <div className="flex flex-col items-center min-h-screen bg-black bg-opacity-90 py-4">
       <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4 md:mb-8 text-yellow-400 text-center">
         Star Wars Films
       </h1>
