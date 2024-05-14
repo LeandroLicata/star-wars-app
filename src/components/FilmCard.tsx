539eaf647544b0f40d611e1044b26c469ba7c772
Leandro Licata <leandrolicata1@gmail.com>
2024-05-14 10:52:49 -0300

    add(-): add detail button to film card

--- src/components/FilmCard.tsx
+++ src/components/FilmCard.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Movie } from "@/interfaces/movie";
+import { useRouter } from "next/navigation";
 
 const FilmCard = ({ title, episode_id }: Movie) => {
+  const router = useRouter();
+
+  const redirectToFilm = () => {
+    router.push(`/films/${episode_id}`);
+  };
   return (
     <div className="flex flex-col items-center justify-center md:mx-4 my-4 md:my-0 border border-yellow-400 rounded-lg bg-black bg-opacity-75 p-4 w-72 md:w-auto">
       <img
         className="h-36 md:h-48 lg:h-56 w-72 md:w-96 lg:w-108 object-cover rounded-lg mb-4"
-        src={`https://via.placeholder.com/300?text=${title}`} // Usar el título de la película en la URL de la imagen de marcador de posición
+        src={"/images/star-wars-movies.jpeg"}
         alt={title}
       />
       <div className="px-6 py-4">
@@ -15,6 +21,12 @@ const FilmCard = ({ title, episode_id }: Movie) => {
           Número de Episodio: {episode_id}
         </p>
       </div>
+      <button
+        onClick={redirectToFilm}
+        className="bg-yellow-400 text-black font-bold py-2 px-4 rounded mt-4 hover:bg-yellow-500"
+      >
+        View Details
+      </button>
     </div>
   );
 };
