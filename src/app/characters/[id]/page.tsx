1b3e1dbee5435143b1f602c07f0884c1730e7f9b
Leandro Licata <leandrolicata1@gmail.com>
2024-05-14 05:14:50 -0300

    add(-): create character detail page

---
+++ src/app/characters/[id]/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import React from "react";
+import useCharacterDetail from "@/hooks/useCharacterDetail";
+import { useRouter } from "next/navigation";
+
+const CharacterDetailPage = ({ params }: { params: { id: string } }) => {
+  const { character, isLoading } = useCharacterDetail(params.id);
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black bg-opacity-90 p-4">
+      <div className="max-w-lg w-full border border-yellow-400 rounded-lg bg-black bg-opacity-75 overflow-hidden shadow-lg">
+        <div className="px-6 py-4">
+          <div className="font-bold text-2xl mb-2 text-yellow-400">
+            {isLoading ? "loading..." : character?.name}
+          </div>
+          <div className="flex justify-center mb-4">
+            <img
+              src="/images/baby-yoda.jpg"
+              alt="Character"
+              className="w-full h-auto rounded-lg"
+            />
+          </div>
+          <div className="mb-2 text-white">
+            <span className="font-bold">Eye color:</span> {character?.eye_color}
+          </div>
+          <div className="mb-2 text-white">
+            <span className="font-bold">Birth year:</span>{" "}
+            {character?.birth_year}
+          </div>
+          <div className="mb-2 text-white">
+            <span className="font-bold">Hair color:</span>{" "}
+            {character?.hair_color}
+          </div>
+          <div className="mb-2 text-white">
+            <span className="font-bold">Height:</span> {character?.height}
+          </div>
+          <div className="mb-2 text-white">
+            <span className="font-bold">Skin color:</span>{" "}
+            {character?.skin_color}
+          </div>
+          <div className="mb-2 text-white">
+            <span className="font-bold">Mass:</span> {character?.mass}
+          </div>
+          <button
+            onClick={handleGoBack}
+            className="bg-yellow-400 text-black font-bold py-2 px-4 rounded mt-4 hover:bg-yellow-500"
+          >
+            Back
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterDetailPage;
